2525c6e3041bc0d17d755e3eae75719823dbf062
Alireza Salimiyan <alirezasalimiyan98@gmail.com>
2025-04-24 21:02:14 +0330

    creating color pallet need further cutomization

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import ThemeToggle from "@/theme/theme-toggle";
 
 export default function Home() {
   return (
-    <div className="grid bg-white dark:bg-black grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid bg-light dark:bg-dark grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <ThemeToggle></ThemeToggle>
     </div>
   );
